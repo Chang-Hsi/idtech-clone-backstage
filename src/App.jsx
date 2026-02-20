3d80e7df68656610c51d8f2092d5b289cc2dd11f
Chang-Hsi <watasiwa8531@gmail.com>
2026-02-20 15:50:00 +0800

    chore: initialize idtech-clone-backstage project

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <main style={{ minHeight: '100vh', display: 'grid', placeItems: 'center', fontFamily: 'sans-serif' }}>
+      <h1>idtech-clone-backstage</h1>
+    </main>
+  )
+}
+
+export default App
